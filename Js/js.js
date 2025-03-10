28d5671adb4fac69d3b1fbedbeb523cb956bcdb7
Chumachingan <148493759+Chumachingan@users.noreply.github.com>
2025-03-10 15:44:34 +0100

    AA
    Inicio y registro

---
+++ Js/js.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("loginModal").style.display = "block";
+}
+
+function cerrarModal() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("loginModal").style.display = "none";
+}
