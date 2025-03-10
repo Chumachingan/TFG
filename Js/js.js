9e6b503295ffe5d3db106c66fef0c3f0308c5b38
Chumachingan <148493759+Chumachingan@users.noreply.github.com>
2025-03-10 17:51:36 +0100

    aa
    Actualización de html y css

--- Js/js.js
+++ Js/js.js
@@ -1,8 +1,10 @@
+// Función para abrir el modal
 function abrirModal() {
     document.getElementById("overlay").style.display = "block";
     document.getElementById("loginModal").style.display = "block";
 }
 
+// Función para cerrar el modal
 function cerrarModal() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("loginModal").style.display = "none";
